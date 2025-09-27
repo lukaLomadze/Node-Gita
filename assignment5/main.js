e08f2820e8fcec36e6c4114b0cff2314c7738773
lukaLomadze <lloma23@freeuni.edu.ge>
2025-09-27 21:49:20 +0400

    started 5

---
+++ assignment5/main.js
@@ -0,0 +1,33 @@
+const log = console.log;
+
+//1
+function deleteProp(){
+
+}
+log(deleteProp())
+
+//2
+function getLeaderboard(){
+
+}
+log(getLeaderboard())
+
+//3
+function getLongestTitle(){
+
+}
+log(getLongestTitle())
+
+
+//4
+function calculateAvareageOfDep(){
+
+}
+log(calculateAvareageOfDep())
+
+
+//5
+function countWords(){
+
+}
+log(countWords())
